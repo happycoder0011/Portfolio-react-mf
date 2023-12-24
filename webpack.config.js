5efbabaf8b7e9cf9d55eb37100a7d93dae9630c7
happycoder0011 <sheetalsinghnew@gmail.com>
2023-12-24 22:33:38 +0530

    build: :rocket: Webpack config for index file modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-
+const path = require('path');
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:3000/",
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
   },
-
   resolve: {
     extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
   },
